95b26add6234f68a887b1b0e23c62ef0ca8c7746
sakshi280304 <141491408+sakshi280304@users.noreply.github.com>
2023-09-12 21:14:45 +0530

    Day2.py

---
+++ Day2.py
@@ -0,0 +1,10 @@
+a = int(input("Enter the value of 'a': "))
+b = int(input("Enter the value of 'b': "))
+
+a = a + b
+b = a - b
+a = a - b
+
+print("After swapping:")
+print("a =", a)
+print("b =", b)
