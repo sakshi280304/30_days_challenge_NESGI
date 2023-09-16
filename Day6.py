bfcdfcfb77b60dca391e688660fefeb1227a7ac1
sakshi280304 <141491408+sakshi280304@users.noreply.github.com>
2023-09-16 23:04:36 +0530

    Day6.py
    Print middle element of an array

---
+++ Day6.py
@@ -0,0 +1,7 @@
+def print_middle_element(array):
+    middle_index = len(array) // 2
+    middle_element = arr[middle_index]
+    print("Middle element of array : ", middle_element)
+
+array = [1, 2, 4, 5]
+print_middle_element(array)
