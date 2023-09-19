3d2734271f6729c8cefbeaf10098eaace8edbc9a
sakshi280304 <141491408+sakshi280304@users.noreply.github.com>
2023-09-19 15:43:13 +0530

    Day7.py
    Program to change constant value

---
+++ Day7.py
@@ -0,0 +1,4 @@
+constant_value = int(input("Enter constant value "))
+print("constant value is ", constant_value)
+constant_value = int(input("Enter new value for constant "))
+print("New constant value is ", constant_value)
