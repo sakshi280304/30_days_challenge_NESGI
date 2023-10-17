d9621f824457ece3733754d3c3110147e60512ad
sakshi280304 <141491408+sakshi280304@users.noreply.github.com>
2023-10-17 21:57:13 +0530

    Day18.py

---
+++ Day18.py
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class App {
+
+    public static String intToRoman(int num) {
+
+        String[] symbols = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+        
+        int[] values = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+
+        StringBuilder result = new StringBuilder();
+
+        for (int i = 0; i < values.length; i++) {
+            while (num >= values[i]) {
+                result.append(symbols[i]);
+                num -= values[i];
+            }
+        }
+
+        return result.toString();
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter an integer: ");
+        int num = scanner.nextInt();
+
+        String romanNumeral = intToRoman(num);
+
+        System.out.println("Roman numeral representation: " + romanNumeral);
+    }
+}
